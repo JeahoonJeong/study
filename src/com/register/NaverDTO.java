c57a214a93c240f23891cc490fde7ce298214454
Unknown <JeahoonJeong789@gmail.com>
2018-07-30 18:04:58 +0900

    2018.07.30 수업 내용
    기존에 만들었던 naver 회원가입창을 이용,
    dto, dao/ list, update, insert, delete창을 사용.
    db와 연결한다.

---
+++ src/com/register/NaverDTO.java
@@ -0,0 +1,62 @@
+package com.register;
+
+public class NaverDTO {
+	
+	private String userid;
+	private String username;
+	private String usernickname;
+	private String userpwd;
+	private String useremail;
+	private String usertel;
+	private String userquestion;
+	private String useranswer;
+	public String getUserid() {
+		return userid;
+	}
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getUsernickname() {
+		return usernickname;
+	}
+	public void setUsernickname(String usernickname) {
+		this.usernickname = usernickname;
+	}
+	public String getUserpwd() {
+		return userpwd;
+	}
+	public void setUserpwd(String userpwd) {
+		this.userpwd = userpwd;
+	}
+	public String getUseremail() {
+		return useremail;
+	}
+	public void setUseremail(String useremail) {
+		this.useremail = useremail;
+	}
+	public String getUsertel() {
+		return usertel;
+	}
+	public void setUsertel(String usertel) {
+		this.usertel = usertel;
+	}
+	public String getUserquestion() {
+		return userquestion;
+	}
+	public void setUserquestion(String userquestion) {
+		this.userquestion = userquestion;
+	}
+	public String getUseranswer() {
+		return useranswer;
+	}
+	public void setUseranswer(String useranswer) {
+		this.useranswer = useranswer;
+	}
+
+}
